095fd76eb1b4016ae844a090cdcc0e9e22f7356a
jeffhinoto <63283396+jeffhinoto@users.noreply.github.com>
2025-09-12 17:03:40 -0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const closeBtn = document.querySelector(".close-btn");
-  const popup = document.querySelector(".img_div");
-
-  if (closeBtn && popup) {
-    closeBtn.addEventListener("click", function () {
-      popup.style.display = "none";
-    });
-  }
+document.getElementById('closeBtn').addEventListener('click', () => {
+  document.getElementById('popup').style.display = 'none';
 });
